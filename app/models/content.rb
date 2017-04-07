9b14db048778e6d44a0094094a19f933724782b6
Remizova Natalia <reminate@gmail.com>
2017-04-07 17:50:08 +0000

    file storage fix

--- app/models/content.rb
+++ app/models/content.rb
@@ -13,8 +13,8 @@ class Content < ApplicationRecord
   ]
 
   has_attached_file :audio,
-                    #storage: :cloudinary,
-                    #cloudinary_resource_type: :video,
+                    storage: :cloudinary,
+                    cloudinary_resource_type: :video,
                     path: ":class/:attachment/:id_partition/:style/:filename"
 
   validates_attachment_content_type :audio, content_type: ALLOWED_AUDIO
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ApplicationRecord
 
   has_attached_file :image,
-                    #storage: :cloudinary,
+                    storage: :cloudinary,
                     path: ":class/:attachment/:id_partition/:style/:filename"
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
